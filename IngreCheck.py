e409367a5efe1108494f5bf5d453effef6b0d55f
Aditya Patil <aadityathere@gmail.com>
2025-03-28 20:30:25 +0530

    Update IngreCheck.py

--- IngreCheck.py
+++ IngreCheck.py
@@ -39,7 +39,7 @@ def get_agent():
 
 def analyze_image(image_path, agent):
     response = agent.run(
-        "Analyze the given image and list ingredients, show Ingredients Information in Table Format",
+        "Analyze the given image and list ingredients in table format",
         images=[image_path],
     )
     return response.content
